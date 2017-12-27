53d53e52c03178306c73c9f96b1abb82748673ac
Bernard Joseph Jean Bruno <brunobernard1996@outlook.com>
2017-12-27 20:54:48 +0400

    Driver UI temporary completed

--- www/js/index.js
+++ www/js/index.js
@@ -1,61 +1,48 @@
 
 $(document).ready(function(){
   $("ons-progress-bar").hide();
-  
+
   $("#push-button").click(function(){
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
-    
+
     $.post("fetch.php",
           {
             username:   email,
-            password: password 
+            password: password
           },
           function(data){
             $("#push-button").fadeOut();
             $("ons-progress-bar").show();
-            
+
             if (data == "success"){
-              
+
               $("ons-progress-bar").hide();
 
               $("#push-button").show();
-              document.querySelector('#myNavigator').pushPage('chauffeur.html', {data: {title: 'Espace Client'}});
+              document.querySelector('#myNavigator').pushPage('vendeur.html', {data: {title: 'Espace Client'}});
               ons.notification.alert('Congratulations!');
             }else{
-            
+
               $("#push-button").fadeIn(1000);
               $("ons-progress-bar").hide();
-              
+
               ons.notification.alert('Incorrect username or password.');
             }
-            
+
           },
           'text'
-  
+
         );
   });
-  
+
 });
 
 
 function confirmation(){
   document.querySelector('#myNavigator').pushPage('confirmation.html', {data: {title: 'Espace Client'}});
-  
+
 }
-
-var showPopover = function(target) {
-  document
-    .getElementById('popover')
-    .show(target);
-};
-
-var hidePopover = function() {
-  document
-    .getElementById('popover')
-    .hide();
-};
-
 
 window.fn = {};
 
@@ -71,3 +58,30 @@ content.load(page)
   .then(menu.close.bind(menu));
 };
 
+//###############################
+
+var showDialog = function (id) {
+  document
+    .getElementById(id)
+    .show();
+};
+
+var hideDialog = function (id) {
+  document
+    .getElementById(id)
+    .hide();
+};
+
+var fromTemplate = function () {
+  var dialog = document.getElementById('dialog-3');
+
+  if (dialog) {
+    dialog.show();
+  }
+  else {
+    ons.createDialog('dialog.html')
+      .then(function (dialog) {
+        dialog.show();
+      });
+  }
+};
