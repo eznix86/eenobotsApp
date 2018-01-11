0899ea1c914c7ac2a6ec38bc02bc7a448dc8cdf9
Bernard Joseph Jean Bruno <brunobernard1996@outlook.com>
2018-01-11 20:52:02 +0400

    Minor fix for UI

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,43 +1,70 @@
 
 $(document).ready(function(){
   $("ons-progress-bar").hide();
-  
+
   $("#push-button").click(function(){
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
-    
+
     $.post("fetch.php",
           {
             username:   email,
-            password: password 
+            password: password
           },
           function(data){
-            
             $("#push-button").fadeOut();
             $("ons-progress-bar").show();
-            
+
             if (data == "success"){
-              
+
               $("ons-progress-bar").hide();
 
               $("#push-button").show();
-              document.querySelector('#myNavigator').pushPage('page2.html', {data: {title: 'Page 2'}});
+              document.querySelector('#myNavigator').pushPage('vendeur.html', {data: {title: 'Espace Client'}});
               ons.notification.alert('Congratulations!');
             }else{
-            
+
               $("#push-button").fadeIn(1000);
               $("ons-progress-bar").hide();
-              
+
               ons.notification.alert('Incorrect username or password.');
             }
-            
+
           },
           'text'
-  
-        );
-  })
-});
 
+        );
+  });
+
+});
+function toggle_search(){
+
+  $(".over-search").toggle();
+
+}
+
+function confirmation(){
+  document.querySelector('#myNavigator').pushPage('confirmation.html', {data: {title: 'Espace Client'}});
+
+}
+
+
+function cart(){
+  document.querySelector('#myNavigator').pushPage('cart.html', {data: {title: 'Espace Client'}});
+}
+
+
+function cart_validation(){
+  document.querySelector('#myNavigator').pushPage('cart-validation.html', {data: {title: 'Espace Client'}});
+}
+
+function ordering(){
+  document.querySelector('#myNavigator').pushPage('ordering.html', {data: {title: 'Espace Client'}});
+}
+
+function order_details(){
+  document.querySelector('#myNavigator').pushPage('order-details.html', {data: {title: 'Espace Client'}});
+}
 
 window.fn = {};
 
@@ -52,4 +79,61 @@ var menu = document.getElementById('menu');
 content.load(page)
   .then(menu.close.bind(menu));
 };
+
+//###############################
+
+var showDialog = function (id) {
+  document
+    .getElementById(id)
+    .show();
+};
+
+var hideDialog = function (id) {
+  document
+    .getElementById(id)
+    .hide();
+};
+
+var fromTemplate = function () {
+  var dialog = document.getElementById('dialog-3');
+
+  if (dialog) {
+    dialog.show();
+  }
+  else {
+    ons.createDialog('dialog.html')
+      .then(function (dialog) {
+        dialog.show();
+      });
+  }
+};
+var cancel_order = function () {
+  var dialog = document.getElementById('cancel-order');
+
+  if (dialog) {
+    dialog.show();
+  }
+  else {
+    ons.createDialog('cancel-order.html')
+      .then(function (dialog) {
+        dialog.show();
+      });
+  }
+};
+//##########################
+var prev = function() {
+  var carousel = document.getElementById('carousel');
+  carousel.prev();
+};
+
+var next = function() {
+  var carousel = document.getElementById('carousel');
+  carousel.next();
+};
+
+ons.ready(function() {
+  var carousel = document.addEventListener('postchange', function(event) {
+    console.log('Changed to ' + event.activeIndex)
+  });
+});
 
